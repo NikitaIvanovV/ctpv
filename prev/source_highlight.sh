da0bd88afabc4c12e57ce5bc3ae2b776aea6e566
Nikita Ivanov <nikita.vyach.ivanov@gmail.com>
2022-06-20 13:58:37 +0500

    Fix broken source-highlight
    Resolves #2

--- prev/source_highlight.sh
+++ prev/source_highlight.sh
@@ -1,6 +1,6 @@
 # text: source-highlight
 
-source-highlight                       \
-	--tab=4 --out-format=esc           \
-	--style=esc256.style --failsafe -i \
-	-- "$f"
+source-highlight                    \
+	--tab=4 --out-format=esc        \
+	--style=esc256.style --failsafe \
+	-i "$f"
