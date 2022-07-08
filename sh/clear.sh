dd9d29b79739d27a027dd0ec29634669f6441339
Nikita Ivanov <nikita.vyach.ivanov@gmail.com>
2022-07-08 14:07:39 +0500

    Fix: forcekittyanim doesn't clear images

--- sh/clear.sh
+++ sh/clear.sh
@@ -1,10 +1,6 @@
 setup_image 1
 
-case "$image_method" in
-	"$image_method_ueberzug")
-		printf '{"action": "remove", "identifier": "preview"}\n' > "$fifo"
-		;;
-	"$image_method_kitty")
-		kitty +kitten icat --clear --transfer-mode file
-		;;
-esac
+[ "$image_method" = "$image_method_ueberzug" ] &&
+	printf '{"action": "remove", "identifier": "preview"}\n' > "$fifo"
+
+is_kitty && kitty +kitten icat --clear --transfer-mode file
