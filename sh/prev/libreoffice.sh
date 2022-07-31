2e0827a87316ef3a5ff9e378a48fbbdd235592d1
Nikita Ivanov <nikita.vyach.ivanov@gmail.com>
2022-07-31 17:16:58 +0500

    Minor

--- sh/prev/libreoffice.sh
+++ sh/prev/libreoffice.sh
@@ -1,15 +1,15 @@
 # office: libreoffice
 
-lo() {
+doc() {
 	# File produced by libreoffice
 	jpg="$(printf '%s\n' "$f" | sed 's|^.*/||; s|\..*$||')"
 
-	libreoffice             \
-		--headless          \
-		--convert-to jpg    \
+	libreoffice \
+		--headless \
+		--convert-to jpg \
 		--outdir "$cache_d" \
 		"$f" >/dev/null &&
 		mv -- "$cache_d/$jpg.jpg" "$cache_f"
 }
 
-convert_and_show_image lo
+convert_and_show_image doc
