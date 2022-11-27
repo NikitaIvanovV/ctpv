dc41738b3235ae4298960f2ee30000bb2094fbd5
Nikita Ivanov <nikita.vyach.ivanov@gmail.com>
2022-11-27 02:55:09 +0100

    Make ffmpeg for optional for audio preview

--- sh/prev/audio.sh
+++ sh/prev/audio.sh
@@ -4,9 +4,10 @@ audio() {
 	ffmpegthumbnailer -i "$f" -s 0 -q 50% -t 10 -o "$cache_f" 2>/dev/null
 }
 
-x="$(ffmpeg -hide_banner -i "$f" 2>&1)"
-
-printf '%s\n' "$x"
-y=$((y + $(printf '%s\n' "$x" | wc -l)))
+if exists ffmpeg; then
+	x="$(ffmpeg -hide_banner -i "$f" 2>&1)"
+	printf '%s\n' "$x"
+	y=$((y + $(printf '%s\n' "$x" | wc -l)))
+fi
 
 convert_and_show_image audio
